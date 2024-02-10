d592ad96ae39485689beb25b232d1a11948090f8
DariuszPakulski2137 <89033390+DariuszPakulski2137@users.noreply.github.com>
2024-02-10 19:22:10 +0100

    Add loading and saving panels inside API

--- API/main.py
+++ API/main.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
@@ -6,12 +8,31 @@ from panelManager import PanelManager
 from panels.panel import Panel
 
 
-app = FastAPI()
 panelManager = PanelManager()
+
+
+def loadPanels():
+    with open("panels.json", "r") as file:
+        panels_from_file = json.loads(file.read())["panels"]
+        for panel in panels_from_file:
+            panelManager.addPanel(Panel(panel["name"], panel["url"]))
+
+
+def savePanels():
+    for panel in panelManager.panels:
+        with open("panels.json", "w") as file:
+            panels_to_json = {"panels": []}
+            panels_to_json["panels"].append({"name": panel.name, "url": panel.url})
+            file.write(json.dumps(panels_to_json, indent=4))
+
+
+loadPanels()
+
 origins = [
     "http://localhost:3000",
 ]
 
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -40,10 +61,12 @@ class PanelPostBody(BaseModel):
 @app.post("/panel")
 def add_new_panel(panel: PanelPostBody):
     panelManager.addPanel(Panel(panel.name, panel.url))
+    savePanels()
     return {'resp': 'OK'}
 
 
 @app.delete("/panel")
 def delete_panel(panelToRemove: PanelPostBody):
     panelManager.deletePanel(panelToRemove.name)
+    savePanels()
     return {"message": f"REMOVED {panelToRemove.name}"}
